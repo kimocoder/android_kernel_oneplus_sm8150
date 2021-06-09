72aff901349a14bfa5c084a030b0f91663cb9071
tony.xue <tony.xue@oneplus.com>
2021-06-09 10:29:05 +0800

    task placement decision (TPD) feature 1/2
    Change-Id: I2f0b3ca6ab3154c6e4f89d2429470ba02b7ce367

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1511,6 +1511,14 @@ struct task_struct {
 #ifdef CONFIG_IM
 	int im_flag;
 #endif
+
+#ifdef CONFIG_TPD
+	int tpd;
+	int dtpd; /* dynamic tpd task */
+	int dtpdg; /* dynamic tpd task group */
+#endif
+
+/* add for cpu distribution statistics */
 	atomic64_t cpu_dist[8];
 	atomic64_t total_cpu_dist[8];
 
